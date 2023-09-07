a6fcd93dd8f0d1dbfdcca0ac1ae2bb8dcbb9019b
mohamedhk2 <mohamedhk2@yahoo.fr>
2023-09-07 10:53:40 +0100

    add php between

--- src/Helpers.php
+++ src/Helpers.php
@@ -7,29 +7,40 @@
  */
 function crypt_response($data, string $key = '$BZMf'): array
 {
-    $data = json_encode($data);
-    $base64 = base64_encode($data);
-    $eq = 0;
-    while ($base64[strlen($base64) - 1] == '=') {
-        $base64 = substr($base64, 0, -1);
-        $eq++;
-    }
-    $base64 = strrev($base64);
-    $base64 = base64_encode($base64);
-    return [
-        $key => $base64,
-        'e' => $eq
-    ];
+	$data = json_encode($data);
+	$base64 = base64_encode($data);
+	$eq = 0;
+	while ($base64[strlen($base64) - 1] == '=') {
+		$base64 = substr($base64, 0, -1);
+		$eq++;
+	}
+	$base64 = strrev($base64);
+	$base64 = base64_encode($base64);
+	return [
+		$key => $base64,
+		'e' => $eq
+	];
 }
 
 function refreshToken()
 {
-    $auth = [];
-    foreach (config('auth.guards') as $guard => $value) {
-        $auth[$guard] = auth($guard)->check();
-    }
-    return response()->json(crypt_response([
-        'token' => csrf_token(),
-        'auth' => $auth,
-    ]));
+	$auth = [];
+	foreach (config('auth.guards') as $guard => $value) {
+		$auth[$guard] = auth($guard)->check();
+	}
+	return response()->json(crypt_response([
+		'token' => csrf_token(),
+		'auth' => $auth,
+	]));
+}
+
+/**
+ * @param int|float $number
+ * @param int|float $min
+ * @param int|float $max
+ * @return bool
+ */
+function between(int|float $number, int|float $min, int|float $max): bool
+{
+	return $number >= $min && $number <= $max;
 }
